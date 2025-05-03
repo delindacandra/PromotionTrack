cbc76d9ee9726ca6b8ff57f93a3a7dce85a14da7
Delinda Candrawati Eka Putri <delindap26@gmail.com>
2025-05-03 12:05:52 +0700

    fix(barang): benahi pemanggilan gambar dan card title

--- resources/views/barang/show.blade.php
+++ resources/views/barang/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="card card-primary card-outline mb-4">
         <div class="card-header">
-            <div class="card-title">Input data barang baru</div>
+            <div class="card-title">Data Barang</div>
         </div>
         <div class="card-body">
             @empty($barang)
@@ -27,7 +27,14 @@
                     </tr>
                     <tr>
                         <th>Gambar barang</th>
-                        <td>{{ $barang->gambar }}</td>
+                        <td>
+                            @if ($barang->gambar)
+                                <img src="{{ asset('storage/' . $barang->gambar) }}" alt="Gambar barang"
+                                    class="img-fluid" width="150px">
+                            @else
+                                <span class="text-danger">Tidak ada gambar</span>
+                            @endif
+                        </td>
                     </tr>
                     <tr>
                         <th>Perubahan stok terakhir</th>
