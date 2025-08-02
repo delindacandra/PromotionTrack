d2430dca4130f5e4934275a57a0965a4f3a20372
Delinda Candrawati Eka Putri <delindap26@gmail.com>
2025-08-02 10:11:39 +0700

    ganti footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,11 +1,11 @@
 <footer class="app-footer">
     <!--begin::To the end-->
-    <div class="float-end d-none d-sm-inline">Footer</div>
+    <div class="float-end d-none d-sm-inline">Version 1.0</div>
     <!--end::To the end-->
     <!--begin::Copyright-->
     <strong>
-        <!-- Copyright &copy; 2014-2024&nbsp;
-        <a href="https://adminlte.io" class="text-decoration-none">AdminLTE.io</a>. -->
+        Copyright &copy; 2025-2026&nbsp;
+        <a href="https://pertaminapatraniaga.com/" class="text-decoration-none">Marketing Operation MOR V Jatimbalinus</a>.
     </strong>
     <!-- All rights reserved. -->
     <!--end::Copyright-->
